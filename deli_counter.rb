f5d2cd8f7f8d8ef55e369beade4c2421128aae75
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-07 22:05:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,11 @@ def take_a_number(katz_deli,name)
   katz_deli<<name
   puts "Welcome, #{name}. You are number katz_deli.find_index(name)+1 in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    katz_deli.each do |name|
+      puts "Now serving #{name[katz_deli.find_index(name)+1]}"
+    end
