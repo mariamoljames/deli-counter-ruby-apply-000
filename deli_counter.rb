ed73f7be2a2026754b94a3447acd007d2e66dd4e
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-07 23:20:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli,name)
+def take_a_number(katz_deli,name,index)
   katz_deli<<name
   puts "Welcome, #{name}. You are number katz_deli.find_index(name)+1 in line."
 end
