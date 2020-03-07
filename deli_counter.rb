7a19e750abdb736d9833b13226b4c35500f4d6a1
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-07 23:10:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty"
   else
     puts "The line is currently:"
-    katz_deli.each do |name|
-      puts "katz_deli.find_index(name)+1. #{name}"
+    katz_deli.each_with_index do |item,idx|
+      puts "#{idx}. #{item}"
     end
   end
 end
