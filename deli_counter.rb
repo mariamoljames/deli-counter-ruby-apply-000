8e090608f0150d19fda12140889f13b72c5d8850
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-08 00:56:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,10 +15,10 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length()==0 || katz_deli.length()==1
+  if katz_deli.length()==0
     puts "There is nobody waiting to be served!"
   else
-    puts "Now serving #{katz_deli[1]}"
-    katz_deli.pop(1)
+    puts "Now serving #{katz_deli[0]}"
+    katz_deli.shift(1)
   end
 end
