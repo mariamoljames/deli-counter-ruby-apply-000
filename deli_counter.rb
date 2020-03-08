bb4a1473117c1473f22a73b4ef94234ba92d65b4
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-08 02:21:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ katz_deli=[]
 def line (katz_deli)
   if katz_deli.count > 1
     puts "The line is currently:"
-    katz_deli.each_with_index {|name, index|  print " #{index +1}. #{name}" }
+    katz_deli.each_with_index do |name, index|  
+      print "#{index +1}. #{name}"
   else
     puts "The line is currently empty."
   end
