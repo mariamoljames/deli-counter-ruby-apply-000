cefc38d38dc5f8cd91c2f25c42ea36df99aadd14
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-08 00:05:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     puts "The line is currently:"
-    katz_deli.each_with_index {|item,index| print "#{index+1}. #{item}"}
-  end
+    katz_deli.each_with_index do |item,index| 
+      print "#{index+1}. #{item}"}
+    end
 end
 
 def take_a_number(katz_deli,name)
