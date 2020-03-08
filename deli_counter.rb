24114c86f948ed625a3fed031fdcbd259f2ea5a6
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-08 00:39:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli=[]
 
 def line(katz_deli)
-  if katz_deli.length==0
+  if katz_deli.length()==0
     puts "The line is currently empty."
   else
     puts "The line is currently: #{katz_deli.map_with_index {|item,index| puts "#{index+1}. #{item}"}}
@@ -10,7 +10,7 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli<<name
-  puts "Welcome, #{name}. You are number #{katz_deli.length()} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
